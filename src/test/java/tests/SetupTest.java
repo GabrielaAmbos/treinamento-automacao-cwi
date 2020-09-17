9c3662ea7f175969d98a644a683be77e285f9696
GabrielaAmbos <gabrielaambos15@gmail.com>
2020-09-16 21:22:51 -0300

    Adicionando cenário de login

--- src/test/java/tests/SetupTest.java
+++ src/test/java/tests/SetupTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utils.Browser;
 import utils.Utils;
 
@@ -12,6 +13,18 @@ public class SetupTest extends BaseTests{
     public void testOpenBrowserAndLoadPage() {
         assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl()));
         System.out.println("Abriuuuu!!!");
+    }
+
+    @Test
+    public void testLogin() {
+        Browser.getCurrentDriver().findElement(By.className("login")).click();
+        System.out.println("Click login");
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains((Utils.getBaseUrl().concat("index.php?controller=authentication&back=my-account"))));
+        Browser.getCurrentDriver().findElement(By.id("email")).sendKeys("gabrielaambos15@gmail.com");
+        Browser.getCurrentDriver().findElement(By.id("passwd")).sendKeys("senha123");
+        Browser.getCurrentDriver().findElement(By.id("SubmitLogin")).click();
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl().concat("index.php?controller=my-account")));
+        assertTrue(Browser.getCurrentDriver().findElement(By.className("page-heading")).getText().contains("MY ACCOUNT"));
     }
 
 }
